ffeea7c3442c959bcce576351b13fa1077436233
Naufal A <spealdm6@gmail.com>
2024-11-06 17:12:59 +0700

    fix: adjust logic multiple

--- tests/assignment-3.js
+++ tests/assignment-3.js
@@ -4,7 +4,17 @@ function sortEvenOddDesc(arr) {
     const odds = arr.filter(num => num % 2 !== 0).sort((a, b) => b - a);
 
     // Combine
-    return [...evens, ...odds];
+    for (let i = 0; i < evens.length; i++) {
+        const newVarEvens = evens[i] * evens[i]
+        console.log(newVarEvens)
+    }
+
+    for (let i = 0; i < odds.length; i++) {
+         newVarOdds = odds[i] * odds[i]
+         console.log(newVarOdds)
+    }
+    
+    // return [...newVarEvens, ...odds];
 }
 
 const inputArray = [3, 2, 5, 1, 8, 9, 6];
